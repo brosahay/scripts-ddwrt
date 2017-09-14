d5142416d5d1c869c4d4143db629e6c088a25197
Rohan Sahay <revosftw@users.noreply.github.com>
2017-09-14 09:28:22 +0530

    added comments

--- adblock.sh
+++ adblock.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+######################################################################
+#
+#  Copyright (c) 2017 revosftw (https://github.com/revosftw)
+#
+######################################################################
+
 # adblock hosts
- curl -L "http://winhelp2002.mvps.org/hosts.txt" >> /tmp/hosts
- curl -L "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=1&mimetype=plaintext&_=207797" >> /tmp/hosts
+curl -L "http://winhelp2002.mvps.org/hosts.txt" >> /tmp/hosts
+curl -L "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=1&mimetype=plaintext&_=207797" >> /tmp/hosts
  
 # alliance adblock hosts
- curl -L "https://raw.githubusercontent.com/revosftw/dd_wrt_scripts/master/adblock/streamride.block" >> /tmp/hosts
-
- stopservice dnsmasq
- sleep 1
- startservice dnsmasq
- echo -e "ad-block actived"
+curl -L "https://raw.githubusercontent.com/revosftw/dd_wrt_scripts/master/adblock/streamride.block" >> /tmp/hosts
+stopservice dnsmasq
+sleep 1
+startservice dnsmasq
+echo -e "ad-block actived"
